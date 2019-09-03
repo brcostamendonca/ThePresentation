2bedfbcea86f4fd096bedeac037daeceffabcc0e
Bruno Mendonça <brcostamendonca@gmail.com>
2019-09-03 15:42:08 +0100

    start

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { AngularComponent } from './angular/angular.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { RouterModule } from '@angular/router';
+import { NotasComponent } from './notas/notas.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CvComponent,
+    AngularComponent,
+    NavMenuComponent,
+    NotasComponent,
+    FetchDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: CvComponent/*HomeComponent*/, pathMatch: 'full' },
+      { path: 'cv', component: CvComponent },
+      { path: 'notas', component: NotasComponent },
+      { path: 'angular', component: AngularComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
