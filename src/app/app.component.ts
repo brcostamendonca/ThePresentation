3b85e8c03633668f03efa7e5687c33271ef917b3
Bruno Mendonça <brcostamendonca@gmail.com>
2020-02-14 14:47:06 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   public constructor(private titleService: Title) {
-    this.titleService.setTitle('The Presentation')
+    this.titleService.setTitle('BCM')
   }
 }
 
